4c05dd64ced21923a18b3982d6df13cf84cf5a05
BreakingNEwss <s6604022620131@email.kmutnb.ac.th>
2025-10-29 15:43:25 +0700

    แก้

--- proj2_20475_20092_20131/src/main.tsx
+++ proj2_20475_20092_20131/src/main.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import PlantDictionary from "./components/PlantDictionary";
+import Payment from "./components/Payment";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dictionary" element={<PlantDictionary />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
