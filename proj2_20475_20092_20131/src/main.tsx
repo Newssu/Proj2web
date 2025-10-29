2c7a761da9c81f1512268592baf1451dd6f27b41
BreakingNEwss <s6604022620131@email.kmutnb.ac.th>
2025-10-29 15:30:23 +0700

    d

--- proj2_20475_20092_20131/src/main.tsx
+++ proj2_20475_20092_20131/src/main.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import PlantDictionary from "./components/PlantDictionary";
-import Payment from "./components/Payment";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/dictionary" element={<PlantDictionary />} />
-        <Route path="/payment" element={<Payment />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
