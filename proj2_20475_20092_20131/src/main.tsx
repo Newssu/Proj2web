d5041e8e4811cd8fcbf156f4eed454ae8a9f0f9f
It's me, Markus <markus.ge2003@gmail.com>
2025-10-29 16:26:39 +0700

    Update main.tsx

--- proj2_20475_20092_20131/src/main.tsx
+++ proj2_20475_20092_20131/src/main.tsx
@@ -35,23 +35,6 @@ function RootRoutes() {
   );
 }
 
-function RootRoutes() {
-  const [cart, setCart] = useState<Cart>(() => loadLocalStorage<Cart>("cart", {}));
-  useLocalStorage("cart", cart);
-
-  const productList = useMemo(() => initialProducts, []);
-
-  return (
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/payment" element={<Payment cart={cart} products={productList} />} />
-      <Route path="/dictionary" element={<PlantDictionary />} />
-      {/* เส้นทางอื่น ๆ ที่ไม่ต้องใช้ props ใส่เพิ่มได้ */}
-
-    </Routes>
-  );
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
