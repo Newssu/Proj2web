1e3cce1c5e8d7ed54a11bb328a2ef7f85aba0886
BreakingNEwss <s6604022620131@email.kmutnb.ac.th>
2025-10-29 15:51:07 +0700

    แก้22

--- proj2_20475_20092_20131/src/main.tsx
+++ proj2_20475_20092_20131/src/main.tsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import PlantDictionary from "./components/PlantDictionary";
 import Payment from "./components/Payment";
+import { products as initialProducts } from "./lib/data";
+import { loadLocalStorage, useLocalStorage } from "./hooks/useLocalStorage";
+import type { Cart } from "./lib/types";
+import PlantDictionary from "./components/PlantDictionary";
 import "./index.css";
+
+function RootRoutes() {
+  const [cart, setCart] = useState<Cart>(() => loadLocalStorage<Cart>("cart", {}));
+  useLocalStorage("cart", cart);
+
+  const productList = useMemo(() => initialProducts, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/payment" element={<Payment cart={cart} products={productList} />} />
+      <Route path="/dictionary" element={<PlantDictionary />} />
+      {/* เส้นทางอื่น ๆ ที่ไม่ต้องใช้ props ใส่เพิ่มได้ */}
+
+    </Routes>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/dictionary" element={<PlantDictionary />} />
-        <Route path="/payment" element={<Payment />} />
-      </Routes>
+      <RootRoutes />
     </BrowserRouter>
   </React.StrictMode>
 );
